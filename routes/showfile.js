dc7ce3b475afd90352c67b7f5c7a1eaace3445b9
puraxa <edinpurivatra@gmail.com>
2019-06-12 15:24:37 +0200

    added notLogged to showfile

--- routes/showfile.js
+++ routes/showfile.js
@@ -5,32 +5,37 @@ var fs = require('fs');
 var lineReader = require('readline');
 var csvstring = require('csv-string');
 router.get('/',(req,res,next)=>{
-    if(!upload.filepath){
-        next({status:404, message:'file must be uploaded first'});
+    if(!req.cookies.token){
+        res.render('notLogged');
     }
-    var currentPage = parseInt(req.query.page);
-    var lineRead = lineReader.createInterface({
-        input: fs.createReadStream(upload.filepath,'utf-8')
-    });
-    var lineCounter = 0;
-    var nesto='';
-    var parsed;
-    lineRead.on('line',line => {
-        lineCounter++;
-        if(lineCounter==1){
-            nesto+= line+ '\n';
+    else
+    {    if(!upload.filepath){
+            next({status:404, message:'file must be uploaded first'});
         }
-        if(lineCounter<currentPage*30+2 && lineCounter>=(currentPage-1)*30+2){
-            nesto += line + '\n';
-        }
-    });
-    var pages = Math.round((lineCounter-1)/30);
-    console.log(pages);
-    lineRead.on('close', () => {
-        parsed = csvstring.parse(nesto);
-        console.log(parsed);
-        res.render('showfile',{result:parsed, pages: Math.round((lineCounter-1)/30)+1,currentPage:currentPage})
-    })
+        var currentPage = parseInt(req.query.page);
+        var lineRead = lineReader.createInterface({
+            input: fs.createReadStream(upload.filepath,'utf-8')
+        });
+        var lineCounter = 0;
+        var nesto='';
+        var parsed;
+        lineRead.on('line',line => {
+            lineCounter++;
+            if(lineCounter==1){
+                nesto+= line+ '\n';
+            }
+            if(lineCounter<currentPage*30+2 && lineCounter>=(currentPage-1)*30+2){
+                nesto += line + '\n';
+            }
+        });
+        var pages = Math.round((lineCounter-1)/30);
+        console.log(pages);
+        lineRead.on('close', () => {
+            parsed = csvstring.parse(nesto);
+            console.log(parsed);
+            res.render('showfile',{result:parsed, pages: Math.round((lineCounter-1)/30)+1,currentPage:currentPage})
+        })
+    }
 })
 
 module.exports = router;
